fdb4437da716518a2928eec2fc30383b1be5016e
Hakan Baran <hakanbaran.developer@gmail.com>
2023-01-25 05:09:13 +0300

    GameCommentModel has been created...

---
+++ Simpra Final Project/Models/GameCommentModel.swift
@@ -0,0 +1,17 @@
+//
+//  GameCommentModel.swift
+//  Simpra Final Project
+//
+//  Created by Hakan Baran on 25.01.2023.
+//
+
+import Foundation
+
+
+struct GameComment {
+    
+    let gameName : String?
+    let gameComment : String?
+    
+    
+}
